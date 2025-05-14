3d6812b911ac019b760bb3112d0288868d97ac19
fouzia nazneen <fouzianazneen236@gmail.com>
2025-05-14 19:23:21 +0530

    done

--- server/Routes/review.js
+++ server/Routes/review.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { getAllReviews, createReview } from "../reviewController.js";
+import {
+  getAllReviews,
+  createReview,
+} from "../controllers/reviewController.js";
 import { authenticate, restrict } from "./../auth/verifyToken.js";
 
-const router = express.Router({mergeParams: true});
+const router = express.Router({ mergeParams: true });
 // /doctor/doctorID/reviews
 
 router
